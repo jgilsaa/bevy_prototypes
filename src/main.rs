947446005b8d7ac878b0c6f03a5ae6d2c466fc61
jgilsaa <jacobgilsaa@hotmail.com>
2024-03-24 18:44:56 +0100

    initial grid mesh

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::ScalingMode};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
+
+mod grid;
+use grid::GridPlugin;
 
 fn main() {
     App::new()
@@ -12,6 +16,46 @@ fn main() {
                 ..default()
             }),
             WorldInspectorPlugin::default(),
+            PanOrbitCameraPlugin,
         ))
+        .add_plugins(GridPlugin)
+        .add_systems(Startup, (setup_camera, setup))
         .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    // Camera
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_translation(Vec3::new(0.0, 1.5, 5.0)),
+            ..default()
+        },
+        PanOrbitCamera::default(),
+    ));
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Ground
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Plane3d::default().mesh().size(8.0, 8.0)),
+            material: materials.add(Color::rgb(0.3, 0.5, 0.3)),
+            ..default()
+        },
+        Name::from("Ground"),
+    ));
+
+    // Light
+    // commands.spawn(PointLightBundle {
+    //     point_light: PointLight {
+    //         shadows_enabled: false,
+    //         ..default()
+    //     },
+    //     transform: Transform::from_xyz(4.0, 8.0, 4.0),
+    //     ..default()
+    // });
 }
